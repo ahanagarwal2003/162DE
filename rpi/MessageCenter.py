fe2997afff32a094dc051192978a836adec221e2
ata4747 <ata47@g.ucla.edu>
2025-05-29 17:15:47 -0700

    rpi that detects stop sign and send to arduino

---
+++ rpi/MessageCenter.py
@@ -0,0 +1,92 @@
+from lib.tlvcodec import Encoder, Decoder
+import time
+import serial
+import ctypes
+import struct
+import sys
+
+NO_OBJECT_DETECTED = 100
+YOLO_OBJECT_DETECTED = 101
+
+class MessageCenter:
+    def __init__(self, serial_port, baudrate=9600, debug=False):
+        self.encoder = Encoder(5, 1024, True) # device id, buffer size, use crc
+        self.decoder = Decoder(self.decoder_callback, True) # use crc
+        
+        self.serial = serial.Serial(serial_port, baudrate=baudrate, timeout=0.01)
+        self.messageCount = 0
+        
+        self.debug = debug
+        
+    def decoder_callback(self, error, frameHeader, tlvs):
+        """Callback function for the decoder."""
+        if self.debug:
+            print(f"Decoder_callback: {error}")
+        
+        # print out tlvs
+        if error.value == 0:
+            # print type length value
+            for i in range(frameHeader.numTlvs):
+                if self.debug:
+                    print(f'Type: {tlvs[i][0]}; length: {tlvs[i][1]}')
+                
+                
+    def processing_tick(self):
+        """To be called every tick to process input and output messages."""
+        # read data from serial port and decode it
+        if self.serial.in_waiting > 0:
+            data = self.serial.read(self.serial.in_waiting)
+            self.decoder.decode(data)
+        
+        # check if there are messages to send
+        if self.messageCount > 0:
+            # send the message
+            length, buffer = self.encoder.wrapupBuffer()
+            self.serial.write(buffer[:length])
+            
+            if self.debug:
+                print(f"Encoder message sent.")
+                print(f"Data [{length}]:")	
+                for i in range(length):
+                    print(f'{buffer[i]:02d}', end = ' ')
+                print("\n")
+            
+            # reset message count
+            self.encoder.reset()
+            self.messageCount = 0
+            
+    def add_message(self, type, length, value):
+        """Add a message to the encoder."""
+        self.encoder.addPacket(type, length, value)
+        self.messageCount += 1
+        
+    def add_yolo_detection(self, object, bbox, confidence):
+        """Add a YOLO detection message to the encoder."""
+        if self.debug:
+            print(f"Adding YOLO detection: {object}, BBox: {bbox}, Confidence: {confidence}")
+            
+        # create a ctypes structure for the bounding box (4 int) and confidence (1 float)
+        class BBox(ctypes.Structure):
+            _fields_ = [("x", ctypes.c_int), ("y", ctypes.c_int), ("w", ctypes.c_int), ("h", ctypes.c_int)]
+        
+        class Detection(ctypes.Structure):
+            _fields_ = [("object", ctypes.c_int), ("bbox", BBox), ("confidence", ctypes.c_float)]
+        
+        detection = Detection()
+        detection.object = ctypes.c_int(object)
+        detection.bbox.x = ctypes.c_int(bbox[0])
+        detection.bbox.y = ctypes.c_int(bbox[1])
+        detection.bbox.w = ctypes.c_int(bbox[2])
+        detection.bbox.h = ctypes.c_int(bbox[3])
+        detection.confidence = ctypes.c_float(confidence)
+        
+        if self.debug:
+            print(f"Detection: {detection.object}, BBox: ({detection.bbox.x}, {detection.bbox.y}, {detection.bbox.w}, {detection.bbox.h}), Confidence: {detection.confidence}")
+    
+        self.add_message(YOLO_OBJECT_DETECTED, ctypes.sizeof(detection), detection)
+        
+    def add_no_object_detected(self):
+        if self.debug:
+            print("No object detected")
+        # don't send anything for now
+        # self.add_message(NO_OBJECT_DETECTED, 0, 0)
---
+++ rpi/main.py
@@ -0,0 +1,144 @@
+# System imports
+import sys, os
+import select
+import ctypes, struct
+import time
+import tty
+import termios
+import argparse
+
+# Package imports
+import cv2
+
+# Custom imports
+from utils import *
+from yolo_utils import *
+from gps_utils import *
+from MessageCenter import MessageCenter
+
+
+def image_processing(message_center):
+    frame = picam2.capture_array()
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+    # Object detection
+    # objects: crosswalk, speedlimit, stop, trafficlight
+    outputs = convert_to_blob(frame, network, 128, 128)
+    bounding_boxes, class_objects, confidence_probs = object_detection(
+        outputs, frame, 0.5
+    )
+
+    # sort the detected objets by confidence and only send the best 2 detections
+    bounding_boxes, class_objects, confidence_probs = sort_by_confidence(
+        2, confidence_probs, bounding_boxes, class_objects
+    )
+
+    if len(bounding_boxes) > 0:
+        for i in range(len(bounding_boxes)):
+            message_center.add_yolo_detection(
+                class_objects[i], bounding_boxes[i], confidence_probs[i]
+            )  # Commented out as add_stop_sign is not defined
+    else:
+        message_center.add_no_object_detected()
+
+
+def gps_processing():
+    data, addr = sock.recvfrom(1024)
+    line = data.decode().strip()
+
+    try:
+        distances_m = list(map(float, line.split(",")))
+    except ValueError:
+        pass
+
+    position, error = trilaterate_2D(distances_m)
+    # if position is not None:
+    #     print(f"[POS] x = {position[0]:.2f} ft, y = {position[1]:.2f} ft, RMSE = {error:.2f} ft")
+
+
+def main():
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description="My program with options")
+    parser.add_argument(
+        "-gps",
+        "--gps",
+        action="store_true",
+        default=False,
+        help="Enable GPS in the program",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        default=False,
+        help="Enable debug mode, printing debug messages",
+    )
+    args = parser.parse_args()
+
+    # initialize the camera and GPS
+    camera_initialization()
+
+    # initialize gps if enabled
+    if args.gps:
+        gps_initialization()
+
+    # initialize the message center
+    message_center = MessageCenter("/dev/ttyUSB0", 9600, args.debug)
+
+    # Welcome message :)
+    print_seal()
+
+    # system start time
+    program_start_time = time.time()
+    time_stamp = program_start_time
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+
+    try:
+        tty.setcbreak(fd)  # or tty.setraw(fd)
+        while True:
+            image_processing(message_center)
+
+            if args.gps:
+                gps_processing()
+
+            # process messages
+            message_center.processing_tick()
+
+            # handle user input
+            if select.select([sys.stdin], [], [], 0)[0]:
+                ch = sys.stdin.read(1)
+                # print(f"You typed: {ch}")
+                if ch == "q":
+                    print("\nUser quit command detected. Exiting program...")
+                    break
+                else:
+                    print(f"\nUnrecognized command: {ch}")
+
+            # delay for a short period to avoid busy waiting
+            time.sleep(0.1)
+
+            # print time elapsed since start and time interval without newline
+            interval = (time.time() - time_stamp) * 1000
+            time_stamp = time.time()
+            print(
+                f"\rTime elapsed: {get_time_millis(program_start_time):.2f} ms; loop interval: {interval:.02f} ms",
+                end="",
+            )
+            print("\b" * 40, end="")  # Clear the line
+
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected. Exiting...")
+        # do some cleanup if necessary
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
